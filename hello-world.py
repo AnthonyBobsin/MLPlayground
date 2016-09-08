3dfae3e7d04ab335c03a2c8231709e29b5f4c07a
Anthony Bobsin <bobsinj@gmail.com>
2016-09-08 00:34:31 -0400

    Integrates pydot and graphviz to visualize decision tree used

--- hello-world.py
+++ hello-world.py
@@ -7,3 +7,10 @@ classifier = tree.DecisionTreeClassifier()
 classifier = classifier.fit(features, labels)
 
 print(classifier.predict([[160, 0]]))
+
+from sklearn.externals.six import StringIO
+import pydotplus as pydot
+dot_data = StringIO()
+tree.export_graphviz(classifier, out_file=dot_data)
+graphs = pydot.graph_from_dot_data(dot_data.getvalue())
+graphs.write_pdf("hello-world.pdf")
