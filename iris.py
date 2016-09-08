eb23bc3a1e536b05993dbc1da74c29082e43c2dd
Anthony Bobsin <bobsinj@gmail.com>
2016-09-08 01:38:59 -0400

    Integrates decision tree classifier to learn from iris dataset and successfully predict sample test data

--- iris.py
+++ iris.py
@@ -1,10 +1,20 @@
+import numpy as np
 from sklearn.datasets import load_iris
+from sklearn import tree
+
 iris = load_iris()
+test_idx = [0, 50, 100]
 
-print(iris.feature_names)
-print(iris.target_names)
-print(iris.data[0])
-print(iris.target[0])
+# training data
+training_target = np.delete(iris.target, test_idx)
+training_data = np.delete(iris.data, test_idx, axis=0)
 
-for i in range(len(iris.target)):
-    print("Example %d: label %s, features %s" % (i, iris.target[i], iris.data[i]))
+# testing data
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+
+classifier = tree.DecisionTreeClassifier()
+classifier = classifier.fit(training_data, training_target)
+
+print(test_target)
+print(classifier.predict(test_data))
